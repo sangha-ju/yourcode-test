4d651d377554c3f19d9474396627c4f60375aa1f
sangha ju <sanghaju@sanghaui-MacBookPro.local>
2021-06-17 18:13:21 +0900

    header 디자인 수정

--- src/components/HeaderBox.js
+++ src/components/HeaderBox.js
@@ -5,7 +5,6 @@ import remoters from "../images/remoters.png";
 const Container = styled.section`
   width: 1450px;
   height: 750px;
-  padding: 17px 61px 0 61px;
   background-color: rgba(98, 115, 232, 0.1);
 `;
 
@@ -15,15 +14,18 @@ const Header = styled.header`
   justify-content: space-between;
   width: 100%;
   height: 50px;
+  padding-top: 100px;
 `;
 
 const Logo = styled.img`
   width: 212px;
+  margin-left: 230px;
 `;
 
 const RegistrationBtn = styled.button`
   width: 240px;
   height: 50px;
+  margin-right: 230px;
   outline: none;
   border: none;
   border-radius: 40px;
@@ -32,6 +34,7 @@ const RegistrationBtn = styled.button`
   color: #ffffff;
   font-weight: 600;
   cursor: pointer;
+  box-shadow: 10px 10px 30px 0 rgba(67, 67, 67, 0.33);
 `;
 
 function HeaderBox() {
